63e0149ef673899ba97206e583529cd642a1795a
Chethana <chethanamurthy@yahoo.com>
2023-09-23 16:30:43 -0500

    Updated Google Calendar Api

--- seed_database.py
+++ seed_database.py
@@ -2,6 +2,15 @@ import requests
 import json
 import datetime
 from tabulate import tabulate
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+from googleapiclient import discovery
+
+# Set up the Google Calendar API client
+credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=[u'https://www.googleapis.com/auth/calendar'])
+calendar_service = build(u'calendar', u'v3', credentials=credentials)
+
 
 def search_events_by_keyword(keyword):
     try:
@@ -34,99 +43,39 @@ def search_events_by_keyword(keyword):
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return []
+    
+def add_event_to_calendar(event):
+    start_date = event['date']
+    event_name = event['name']
+    event_location = event['venue']
+
+    event_body = {
+        'summary': event_name,
+        'location': event_location,
+        'description': f"Event: {event_name}\nLocation: {event_location}",
+        'start': {
+            'date': start_date,
+            'timeZone': 'America/Los_Angeles',  # Adjust the timezone as needed
+        },
+        'end': {
+            'date': start_date,
+            'timeZone': 'America/Los_Angeles',
+        },
+    }
+
+    # Insert the event into the Google Calendar
+    calendar_service.events().insert(calendarId='primary', body=event_body).execute()
 
 def get_unique_venues(events):
-    return list(set(event['venue'] for event in events))
+    return list(set(event['venue']for event in events))
+    #return [event['venue'] for event in events]
 
 
 if __name__ == '__main__':
     keyword = input("Enter a keyword to search for events: ")
     search_results = search_events_by_keyword(keyword)
 
+    for event in search_results:
+        add_event_to_calendar(event)
 
-# def search_events_by_keyword(keyword):
-#     API_KEY = 'QhNYM6o1exkzTChcHqRMrN6LGr7BSR7b'
-#     endpoint = 'https://app.ticketmaster.com/discovery/v2/events'
-    
-#     params = {
-#         'apikey': API_KEY,
-#         'keyword': keyword,
-#         'locale': '*',
-#         'city': 'Dallas'
-#     }
-
-#     try:
-#         response = requests.get(endpoint, params=params)
-#         response.raise_for_status()
-
-#         data = response.json()
-        
-#         # Extract and print event data
-#         events = []
-#         if '_embedded' in data and 'events' in data['_embedded']:
-#             event_data = data['_embedded']['events']
-#             for event in event_data:
-#                 event_id = event['id']
-#                 name = event['name']
-#                 venue = event['_embedded']['venues'][0]['name']
-#                 events.append([event_id, name, venue])
-
-#         if events:
-#             # Display events in a table
-#             headers = ["Event ID", "Event Name", "Venue"]
-#             print(tabulate(events, headers=headers, tablefmt="grid"))
-#         else:
-#             print("No events found for the given keyword.")
-
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error: {e}")
-
-# if __name__ == '__main__':
-#     keyword = input("Enter a keyword to search for events: ")
-#     search_results = search_events_by_keyword(keyword)
-
-
-# Replace 'YOUR_API_KEY' with your actual Ticketmaster API key
-# API_KEY = 'QhNYM6o1exkzTChcHqRMrN6LGr7BSR7b'
-
-# Set the base URL for the Ticketmaster API
-# BASE_URL = 'https://app.ticketmaster.com/discovery/v2/'
-
-# Define a function to search for events by keyword
-# def search_events_by_keyword(keyword):
-    # endpoint = 'events.json'
-    # params = {
-    #     'keyword': keyword,
-    #     'apikey': API_KEY
-    # }
-
-    # try:
-    #     response = requests.get('https://app.ticketmaster.com/discovery/v2/events?apikey=QhNYM6o1exkzTChcHqRMrN6LGr7BSR7b&keyword=kids&locale=*&city=Dallas')
-    #     response.raise_for_status()
-
-    #     data = response.json()
-    #     print(f'Data Response', data)    
-        
-    #     attribute_value = data.get("events")
-    #     print(f'Data Name', attribute_value)    
-
-    #     # Extract and print event data
-    #     if '_embedded' in data:
-    #         embeddedd_data = data['_embedded']
-    #         if 'events' in embeddedd_data:
-    #             event_data = embeddedd_data['events']
-    #             for event in event_data:
-                    
-    #                 event_id = event['id']
-    #                 name = event['name']
-    #                 print(f'Event ID: ', event_id)
-    #                 print(f'Event Name: ', name)
-    #                 # date = datetime.datetime.strptime(event_data['dates']['start']['localDate'], '%Y-%m-%d')
-    #                 venue = event['_embedded']['venues'][0]['name']
-
-    # except requests.exceptions.RequestException as e:
-    #     print(f"Error: {e}")
-
-# if __name__ == '__main__':
-#     keyword = input("Enter a keyword to search for events: ")
-#     search_events_by_keyword(keyword)
+    print("Events added to your Google Calendar.")
--- server.py
+++ server.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-#from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from seed_database import search_events_by_keyword, get_unique_venues
+from seed_database import search_events_by_keyword, get_unique_venues,add_event_to_calendar
+import seed_database
+import os
+#from apis import ticketmaster
+from flask import Flask, render_template, jsonify, request, session, flash
+from model import connect_to_db , db, User, Meeting
+from datetime import datetime
 
 app = Flask(__name__)
-
-    # Sample events data
-    # events = [
-    #     {"name": "Kids Art Class", "date": "2023-09-15"},
-    #     {"name": "Children's Storytime", "date": "2023-09-20"},
-    #     {"name": "Family Movie Night", "date": "2023-09-25"},
-    #     # Add more events here
-    # ]
+   
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -26,6 +24,26 @@ def home():
         print(f"venue list: ", results)
 
         return render_template('home.html', events=results)
+
+@app.route("/add_to_calendar", methods=["POST"])
+def add_to_calendar():
+    try:
+        data = request.json
+        event_ids = data.get("event_ids", [])
+        
+        if not event_ids:
+            return jsonify({"message": "No events selected."}), 400        
+        
+        # Get the events corresponding to the selected event IDs
+        selected_events = [event for event in seed_database.search_results  if event['id'] in event_ids]
+        
+        # Add selected events to the Google Calendar
+        for event in selected_events:
+            add_event_to_calendar(event)
+
+        return jsonify({"message": "Selected events added to your Google Calendar."}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
 
 
 if __name__ == '__main__':
